40b80f42077f0d11c9d75d91369d719819b7d4a6
Parul123228 <wantedgamingyt12@gmail.com>
2025-07-28 12:31:05 +0530

    Update layout.tsx
    Fix google verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,11 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/lib/theme-provider';
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
 import { AuthProvider } from '@/contexts/auth-context';
+import Head from 'next/head';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -24,13 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <meta name="google-site-verification" content="rUDmKEaZtlUWj-5HVco63ZPQziQqBsUPbvnxXxBhLEg" />
+      </Head>
       <body className={`${inter.variable} font-body antialiased`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           <AuthProvider>
             <div className="relative flex flex-col min-h-screen w-full">
               <Header />
